99e269f6e1a1b4ed8eb2de67d80327b147e06330
Walter Ribeiro <walterjunioranalise@gmail.com>
2019-09-29 10:51:37 -0300

    Challenge 02

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: process.env.APP_SECRET, // '3159251745e46625762671090f6a0896',
+  expiresIn: '7d',
+};
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: process.env.SENTRY_DSN, // 'https://c5752bad5eda4abb9a0998874544076e@sentry.io/1757672',
+};
